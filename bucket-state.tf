fa9dc588e1c38c4db37816ef02d3e579469e6f1d
MadhavSake <109482275+MadhavSake@users.noreply.github.com>
2026-02-10 16:41:55 +0530

    Update bucket-state.tf

--- bucket-state.tf
+++ bucket-state.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "terraform_state" {
-  name                        = "coversure-stage-state-bucket" # Replace with a globally unique name
+  name                        = "state-bucket-name" # Replace with a globally unique name
   location                    = "asia-south1"              # Specify the bucket location (India region)
   storage_class               = "STANDARD"                 # Use "STANDARD", "NEARLINE", etc.
   uniform_bucket_level_access = true                       # Recommended for simpler access control
@@ -20,4 +20,5 @@ resource "google_storage_bucket" "terraform_state" {
 
 output "bucket_name" {
   value = google_storage_bucket.terraform_state.name
+
 }
