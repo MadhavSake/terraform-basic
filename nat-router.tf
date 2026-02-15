289b8a9668e9e76c7f522d79a5b50dc484ec7b7f
MadhavSake <109482275+MadhavSake@users.noreply.github.com>
2026-02-15 13:56:01 +0530

    Update nat-router.tf

--- nat-router.tf
+++ nat-router.tf
@@ -1,13 +1,4 @@
 ############################################
-# Enable Compute API (if not already enabled)
-############################################
-
-resource "google_project_service" "compute" {
-  service = "compute.googleapis.com"
-  project = var.project
-}
-
-############################################
 # Static External IP for NAT
 ############################################
 
@@ -43,9 +34,9 @@ resource "google_compute_router" "router" {
 ############################################
 
 resource "google_compute_router_nat" "nat" {
-  name   = var.nat_name
-  router = google_compute_router.router.name
-  region = var.region
+  name    = var.nat_name
+  router  = google_compute_router.router.name
+  region  = var.region
   project = var.project
 
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
@@ -55,20 +46,17 @@ resource "google_compute_router_nat" "nat" {
     google_compute_address.natstage.self_link
   ]
 
-  # Private Subnet 1
   subnetwork {
     name                    = google_compute_subnetwork.private_1.id
     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   }
 
-  # Private Subnet 2
   subnetwork {
     name                    = google_compute_subnetwork.private_2.id
     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   }
 
   depends_on = [
-    google_compute_router.router,
-    google_compute_address.natstage
+    google_compute_router.router
   ]
 }
