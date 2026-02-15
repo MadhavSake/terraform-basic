f3dcae95c58ff2684267f8e70eb0b88cf524e40d
MadhavSake <109482275+MadhavSake@users.noreply.github.com>
2026-02-15 11:12:57 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -8,73 +8,45 @@ resource "google_project_service" "container" {
 }
 
 ############################################
-# GKE Cluster
+# Cheapest GKE Cluster (POC)
 ############################################
 
 resource "google_container_cluster" "primary" {
   name     = "${var.project}-gke-cluster"
-  location = var.region
+  location = var.zone   # Zonal = cheaper
   project  = var.project
 
   network    = google_compute_network.main.id
   subnetwork = google_compute_subnetwork.private_1.id
 
-  # Keep default node pool
+  # Only 1 node
   remove_default_node_pool = false
   initial_node_count       = 1
 
   deletion_protection = false
+  networking_mode     = "VPC_NATIVE"
 
-  # VPC Native (Required for secondary ranges)
   ip_allocation_policy {
     cluster_secondary_range_name  = "pods-range"
     services_secondary_range_name = "services-range"
   }
 
-  # Recommended settings
-  networking_mode = "VPC_NATIVE"
-
-  depends_on = [
-    google_project_service.container
-  ]
-}
-
-############################################
-# Additional Node Pool
-############################################
-
-resource "google_container_node_pool" "gke_node_pool" {
-  name     = "gke-node-pool"
-  location = var.region
-  cluster  = google_container_cluster.primary.name
-  project  = var.project
-
-  node_count = 2
-
   node_config {
-    machine_type = "e2-medium"
+    machine_type = "e2-micro"   # cheapest machine
+
+    # Spot instance (very cheap)
+    spot = true
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
-
-    labels = {
-      env = "dev"
-    }
-
-    tags = ["gke-node"]
 
     metadata = {
       disable-legacy-endpoints = "true"
     }
   }
 
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
   depends_on = [
-    google_container_cluster.primary
+    google_project_service.container
   ]
 }
