37a1c9d0c332f1ccb470b00946f2c625f85a13b6
MadhavSake <109482275+MadhavSake@users.noreply.github.com>
2026-02-16 10:07:32 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,47 +1,31 @@
-provider "google" {
-  project = var.project
-  region  = var.region
-}
-
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
   }
-
-  required_version = ">= 1.3.0"
 }
 
-# ------------------------------
-# Enable Compute API
-# ------------------------------
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
 resource "google_project_service" "compute" {
-  service  = "compute.googleapis.com"
-  project  = var.project
-
-  disable_on_destroy         = false
-  disable_dependent_services = false
+  project                    = var.project
+  service                    = "compute.googleapis.com"
+  disable_dependent_services = true
 }
 
-# ------------------------------
-# Enable GKE API
-# ------------------------------
 resource "google_project_service" "container" {
-  service  = "container.googleapis.com"
-  project  = var.project
-
-  disable_on_destroy = false
+  project = var.project
+  service = "container.googleapis.com"
 }
 
-# ------------------------------
-# Enable Cloud Resource Manager API
-# (DO NOT disable this ever)
-# ------------------------------
 resource "google_project_service" "cloudresourcemanager" {
-  service  = "cloudresourcemanager.googleapis.com"
-  project  = var.project
-
-  disable_on_destroy = false
+  project = var.project
+  service = "cloudresourcemanager.googleapis.com"
 }
