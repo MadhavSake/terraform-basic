355e7d22ab7ca92676439cf489a4e888e4f96df7
MadhavSake <109482275+MadhavSake@users.noreply.github.com>
2026-02-18 10:57:20 +0530

    Update nat-router.tf

--- nat-router.tf
+++ nat-router.tf
@@ -2,8 +2,8 @@
 # Static External IP for NAT
 ############################################
 
-resource "google_compute_address" "natstage" {
-  name         = "natstage"
+resource "google_compute_address" "natstage-gke" {
+  name         = "natstage-gke"
   project      = var.project
   region       = var.region
   address_type = "EXTERNAL"
@@ -60,3 +60,4 @@ resource "google_compute_router_nat" "nat" {
     google_compute_router.router
   ]
 }
+
