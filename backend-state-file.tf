2143b39a15932d1ceff40989e304f08c08c3c6d2
MadhavSake <109482275+MadhavSake@users.noreply.github.com>
2026-02-24 17:18:36 +0530

    Update backend-state-file.tf

--- backend-state-file.tf
+++ backend-state-file.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket         = "terrafor-state-automation-480006" # Replace with your bucket name
+    bucket         = "add-your-bucket-name" # Replace with your bucket name
     credentials    = "gcp-project-service-account-json-key.json" # Replace with the path to your GCP service account JSON key
     prefix         = "terraform/state/terrform-basic"             # Path to state file within the bucket
   }
 }
-
