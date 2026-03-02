c6ae8edf8d760a12453e7371081988ae178ba3b6
MadhavSake <109482275+MadhavSake@users.noreply.github.com>
2026-03-02 14:53:44 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "madhav-terraform-state-582802577279"
+    bucket         = "<your-bucket-name>"
     key            = "aws-basic/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
+
